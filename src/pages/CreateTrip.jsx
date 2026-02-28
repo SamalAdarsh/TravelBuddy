cdd7c25cd9f824fed513adaf7d99eb97d58aa413
Adarsh <codewithadarsh007@gmail.com>
2026-02-28 17:14:05 +0530

    created the destination & days art in create page

--- src/pages/CreateTrip.jsx
+++ src/pages/CreateTrip.jsx
@@ -1,5 +1,6 @@
 import { placesApiKey } from "@/lib/Constants";
-import React, { useState } from "react";
+import { Calendar } from "lucide-react";
+import React, { useEffect, useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 const CreateTrip = () => {
@@ -12,11 +13,20 @@ const CreateTrip = () => {
     budget: "",
   });
 
-  
+  const handleInputChange = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
 
   //Planner Form View
   return (
-    <div className="mx-padd-container flexCenter pt-18 h-screen ">
+    <div className="max-padd-container flexCenter pt-18 h-screen">
       {/* Container */}
       <div className="w-full max-w-3xl min-h-[86vh] sm:min-h-[80vh] bg-white rounded-3xl shadow-xl border border-gray-100 overflow-hidden flex flex-col">
         {/* Progress Bar */}
@@ -37,31 +47,49 @@ const CreateTrip = () => {
             ))}
           </div>
           <div className="flex-1 flex flex-col pt-2 sm:pt-12">
-
             {/* Step1: Destination & Days */}
-            {
-                step == 1 && (
+            {step == 1 && (
+              <div className="space-y-6">
+                <div className="text-center mb-8">
+                  <h3 className="mb-2">Where's your next adventure?</h3>
+                  <p>Select your destination and duration (max 7 days).</p>
+                </div>
 
-                    <div>
-                        <div>
-                            <h3>Where's your next adventure?</h3>
-                            <p>Select your destination and duration (max 5 days).</p>
-                        </div>
-
-                        <div>
-                            <label htmlFor="">
-                                Destination
-                            </label>
-                            <GooglePlacesAutocomplete
-                             apiKey={placesApiKey}
-                            />
-                        </div>
-
+                <div className="space-y-4">
+                  <label className="text-sm font-medium ml-1">
+                    Destination
+                  </label>
+                  <GooglePlacesAutocomplete
+                    apiKey={placesApiKey}
+                    selectProps={{
+                      value: formData.destination,
+                      onChange: (v) => handleInputChange("destination", v),
+                      className: "w-full",
+                      placeholder: "Search for a city...",
+                    }}
+                  />
+                  <div className="grid grid-cols-1 gap-4 pt-2">
+                    <label className="text-sm font-medium">
+                      How many Days?
+                    </label>
+                    <div className="relative">
+                      <Calendar className="absolute left-3 top-3 h-5 w-5 text-gray-400 " />
+                      <input
+                        type="number"
+                        min={1}
+                        max={7}
+                        placeholder="1"
+                        value={formData.noOfDays}
+                        onChange={(e) =>
+                          handleInputChange("noOfDays", e.target.value)
+                        }
+                        className="block w-full pl-10 pr-3 py-3 border border-gray-200 rounded-xl focus:ring-2 focus:ring-indigo-500 transition-all bg-gray-50"
+                      />
                     </div>
-
-                    
-                )
-            }
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
