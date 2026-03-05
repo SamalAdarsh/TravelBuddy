1a03e07a172d85dfcc3236b11c629c8469e8f54d
Adarsh <codewithadarsh007@gmail.com>
2026-03-05 20:32:35 +0530

    Re-adding lib folder with verified lowercase casing

---
+++ src/lib/constants.js
@@ -0,0 +1,6 @@
+///<reference types="vite/client" />
+export const placesApiKey = import.meta.env.VITE_GOOGLE_PLACES_API_KEY
+
+export const geminiApiKey = import.meta.env.VITE_GEMINI_API_KEY
+
+export const googleAuthKey = import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID
