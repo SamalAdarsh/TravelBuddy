1af35dc3d8319bc276ff80e2406cd7bdc92342a0
Adarsh <codewithadarsh007@gmail.com>
2026-03-05 20:14:16 +0530

    constants.js

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { googleAuthKey } from './lib/constants';
+import { googleAuthKey } from './lib/constants.js';
 
 
 createRoot(document.getElementById('root')).render(
